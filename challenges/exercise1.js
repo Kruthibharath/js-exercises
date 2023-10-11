8ae099f66dbf169761f3bb2cd51fb6da46bf141b
Kruthibharath <kruthi.bharath@gmail.com>
2023-10-11 14:08:48 +0100

    Implemented exercise1 assessment

--- challenges/exercise1.js
+++ challenges/exercise1.js
@@ -1,24 +1,36 @@
 export function getFillings(sandwich) {
-	if (sandwich === undefined) throw new Error('ingredients is required');
-	// Your code here!
+  if (sandwich === undefined) throw new Error("ingredients is required");
+  // Your code here!
+  //   if (sandwich && sandwich.fillings && Array.isArray(sandwich.fillings)) {
+  //     return sandwich.fillings;
+  //   } else {
+  //     return [];
+  //   }
+  return sandwich && sandwich.fillings && Array.isArray(sandwich.fillings)
+    ? sandwich.fillings
+    : [];
 }
 
 export function isFromManchester(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
+  return person.city === "Manchester";
 }
 
 export function getBusNumbers(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
+  return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
-	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+  if (arr === undefined) throw new Error("arr is required");
+  // Your code here!
+  return arr.filter((animal) => animal === "sheep").length;
 }
 
 export function hasMPostCode(person) {
-	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+  if (person === undefined) throw new Error("person is required");
+  // Your code here!
+  return person.address.postCode.charAt(0).toUpperCase() === "M";
 }
