c198b71e5c8cff2f04407b5443c5237fafb5d5be
Kruthibharath <kruthi.bharath@gmail.com>
2023-10-13 19:28:06 +0100

    Completed exercise2 and exercise3

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -1,26 +1,45 @@
 export function getSquares(nums) {
-	if (nums === undefined) throw new Error('nums is required');
-	// Your code here!
+  if (nums === undefined) throw new Error("nums is required");
+  // Your code here!
+  return nums.length === 0 ? [] : nums.map((number) => number * number);
 }
 
 export function camelCaseWords(words) {
-	if (words === undefined) throw new Error('words is required');
-	// Your code here!
+  if (words === undefined) throw new Error("words is required");
+  // Your code here!
+  const camelCased =
+    words[0] +
+    words
+      .slice(1)
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join("");
+  return camelCased;
 }
 
 export function getTotalSubjects(people) {
-	if (people === undefined) throw new Error('people is required');
-	// Your code here!
+  if (people === undefined) throw new Error("people is required");
+  // Your code here!
+  return people.reduce((total, person) => total + person.subjects.length, 0);
 }
 
 export function checkIngredients(menu, ingredient) {
-	if (menu === undefined) throw new Error('menu is required');
-	if (!ingredient) throw new Error('ingredient is required');
-	// Your code here!
+  if (menu === undefined) throw new Error("menu is required");
+  if (!ingredient) throw new Error("ingredient is required");
+  // Your code here!
+  for (const item of menu) {
+    if (item.ingredients.includes(ingredient)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export function duplicateNumbers(arr1, arr2) {
-	if (arr1 === undefined) throw new Error('arr1 is required');
-	if (arr2 === undefined) throw new Error('arr2 is required');
-	// Your code here!
+  if (arr1 === undefined) throw new Error("arr1 is required");
+  if (arr2 === undefined) throw new Error("arr2 is required");
+  // Your code here!
+  const set1 = new Set(arr1);
+  const checkDuplicates = arr2.filter((number) => set1.has(number));
+  const unquieValues = Array.from(new Set(checkDuplicates));
+  return unquieValues.sort((a, b) => a - b);
 }
